37d04c1cddd5fd5207eaf251da3355d1e4c22e94
othon <162609004+othonhugo@users.noreply.github.com>
2025-10-27 01:23:00 -0300

    feat(interface): add abstract 'File' class for file operations

--- src/mydb/interface/__init__.py
+++ src/mydb/interface/__init__.py
@@ -1,7 +1,9 @@
+from .file import File
 from .index import Index
 from .storage import StorageEngine
 
 __all__ = [
+    "File",
     "Index",
     "StorageEngine",
 ]
---
+++ src/mydb/interface/file.py
@@ -0,0 +1,38 @@
+from abc import ABC, abstractmethod
+from os import SEEK_SET
+from typing import Self
+
+
+class File(ABC):
+    @abstractmethod
+    def write(self, data: bytes) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def read(self, size: int = -1) -> bytes:
+        raise NotImplementedError
+
+    @abstractmethod
+    def seek(self, offset: int, whence: int = SEEK_SET) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def tell(self) -> int:
+        raise NotImplementedError
+
+    @abstractmethod
+    def close(self) -> None:
+        raise NotImplementedError
+
+    @property
+    @abstractmethod
+    def closed(self) -> bool:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __enter__(self) -> Self:
+        raise NotImplementedError
+
+    @abstractmethod
+    def __exit__(self, *_) -> None:
+        raise NotImplementedError
